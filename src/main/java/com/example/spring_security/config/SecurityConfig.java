2b76d045e93cbf8176fd2ee0e7ad81d2d9abc3a8
choidongkuen <danaver12@daum.net>
2023-07-09 01:57:22 +0900

    FEAT : SecurityConfig 구현
    H2 데이터베이스 접근에는 인증 과정 요구 x

---
+++ src/main/java/com/example/spring_security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.spring_security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
+    }
+}
