8b2f1ab52d99ac1545ca3eb88e391fd088492f9d
choidongkuen <danaver12@daum.net>
2023-07-09 02:53:15 +0900

    FEAT : domain 구현

--- src/main/java/com/example/spring_security/domain/entity/Member.java
+++ src/main/java/com/example/spring_security/domain/entity/Member.java
@@ -5,8 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Getter
@@ -14,13 +21,60 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Member {
+public class Member implements UserDetails {
 
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mbr_id")
     private Long id;
 
+    @Column(name = "mbr_email",nullable = false)
+    private String email;
+
+    @Column(name = "mbr_name",nullable = false)
     private String name;
 
+    @Column(name = "mbr_password",nullable = false)
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/spring_security/domain/repository/MemberRepository.java
+++ src/main/java/com/example/spring_security/domain/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.spring_security.domain.repository;
 import com.example.spring_security.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
 }
